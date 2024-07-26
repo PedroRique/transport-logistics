ecf8006a8cd01d783e2285427a6b670661309627
Pedro Rique <prique@patientnow.com>
2024-07-26 09:16:53 -0300

    feat: Update delivery list component with pagination and filtering

--- src/app/deliveries/delivery-list/delivery-list.component.ts
+++ src/app/deliveries/delivery-list/delivery-list.component.ts
@@ -1,14 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewChild } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  OnDestroy,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import { DeliveryState } from '../../../state/delivery.reducer';
 import { selectAllDeliveries } from '../../../state/delivery.selectors';
 import { Delivery, DeliveryStatus } from '../../models/delivery.model';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-delivery-list',
@@ -24,7 +32,7 @@ import { Delivery, DeliveryStatus } from '../../models/delivery.model';
   templateUrl: './delivery-list.component.html',
   styleUrl: './delivery-list.component.scss',
 })
-export class DeliveryListComponent {
+export class DeliveryListComponent implements OnInit, AfterViewInit, OnDestroy {
   deliveries: Delivery[] = [];
   filteredDeliveries = new MatTableDataSource<Delivery>();
   selectedDriver: string = '';
@@ -42,18 +50,32 @@ export class DeliveryListComponent {
     'cliente_destino',
     'status_entrega',
   ];
+  private subscriptions: Subscription = new Subscription();
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private store: Store<DeliveryState>) {}
+  constructor(
+    private store: Store<DeliveryState>,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
-    this.store.select(selectAllDeliveries).subscribe((data) => {
-      this.deliveries = data;
-      this.filteredDeliveries.data = this.deliveries;
-      this.filteredDeliveries.paginator = this.paginator;
-      this.drivers = [...new Set(data.map((d) => d.motorista.nome))];
-    });
+    this.subscriptions.add(
+      this.store.select(selectAllDeliveries).subscribe((data) => {
+        this.deliveries = data;
+        this.filteredDeliveries.data = this.deliveries;
+        this.drivers = [...new Set(data.map((d) => d.motorista.nome))];
+        this.cdr.detectChanges();
+      })
+    );
+  }
+
+  ngAfterViewInit(): void {
+    this.filteredDeliveries.paginator = this.paginator;
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
   applyFilter() {
